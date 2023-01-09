cfe6c58a137a170049ac7971dd346cc95e354936
PrashantChede <chedeprashant2022@gmail.com>
2023-01-09 20:14:20 +0530

    done11

---
+++ Day10/Prashant/DLL/DatabaseManager.cs
@@ -0,0 +1,39 @@
+﻿namespace DLL;
+using BOL;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+public class DatabaseManager
+{
+
+public static List<Admin> GetAllAdminsFromFile()
+    {
+       // Console.WriteLine("inside DAL");
+        string loginFile=@"D:\cdac_project\.Net\Prashant\HighwayHelp\wwwroot\file\adminLoginCradentials.json";
+        var readData=System.IO.File.ReadAllText(loginFile);
+        List<Admin> allAdmins=JsonSerializer.Deserialize<List<Admin>>(readData);
+        return allAdmins;
+    }
+
+
+    public static List<Hospital> GetAllHospitalsFromFile()
+    {
+       // Console.WriteLine("inside DAL");
+        string hospitalFile=@"D:\cdac_project\.Net\Prashant\HighwayHelp\wwwroot\file\hospitalDetails.json";
+        var readData=System.IO.File.ReadAllText(hospitalFile);
+        List<Hospital> allHospitals=JsonSerializer.Deserialize<List<Hospital>>(readData);
+        return allHospitals;
+    }
+
+
+     public static bool InsertIntoFile(List<Hospital> allHospitals)
+    {
+       // Console.WriteLine("inside DAL");
+       string hospitalFile=@"D:\cdac_project\.Net\Prashant\HighwayHelp\wwwroot\file\hospitalDetails.json";
+        var jsonData=JsonSerializer.Serialize<List<Hospital>>(allHospitals); 
+        System.IO.File.WriteAllText(hospitalFile,jsonData);
+        
+        return true;
+    }
+
+}
---
+++ Day10/Prashant/HighwayHelp/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HighwayHelp.Models;
+using BLL;
+using BOL;
+using System.Collections.Generic;
+
+namespace HighwayHelp.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // HelpManager hm=new HelpManager();
+        // List<Hospital> allHospitals=hm.GetAllHospitals();
+        // this.ViewData["hospitals"]=allHospitals;
+
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        // HelpManager hm=new HelpManager();
+        // List<Hospital> allHospitals=hm.GetAllHospitals();
+        // this.ViewData["hospitals"]=allHospitals;
+
+        return View();
+    }
+
+    public IActionResult WelcomeAdmin()
+    {
+        return View();
+    }
+
+    public IActionResult Validate(string username,string password)
+    {
+        // Console.WriteLine("inside controllers Deatils");
+        //  HelpManager hm=new HelpManager();
+        //  Hospital hospital=hm.GetHospital(id);
+        //  this.ViewData["hospital"]=hospital;
+        HelpManager hm=new HelpManager();
+        if(hm.DoValidate(username,password))
+        {
+
+            return Redirect("WelcomeAdmin");
+        }
+         return View();
+
+    } 
+
+   
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
